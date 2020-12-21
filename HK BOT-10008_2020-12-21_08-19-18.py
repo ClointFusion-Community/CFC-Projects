7327d4e7fb4014661a138625307d9d8cf59d28b8
ClointFusion-Community <ClointFusion@cloint.com>
2020-12-21 08:19:19 +0530

    HK BOT-10008

---
+++ HK BOT-10008_2020-12-21_08-19-18.py
@@ -0,0 +1,6 @@
+i;m;p;o;r;t; ;C;l;o;i;n;t;F;u;s;i;o;n; ;a;s; ;c;f;
+;c;f;.;l;a;u;n;c;h;_;a;n;y;_;e;x;e;_;b;a;t;_;a;p;p;l;i;c;a;t;i;o;n;(;';n;o;t;e;p;a;d;';);
+;c;f;.;k;e;y;_;h;i;t;_;e;n;t;e;r;(;);
+;c;f;.;k;e;y;_;w;r;i;t;e;_;e;n;t;e;r;(;';H;K;H;R;';,;k;e;y;=;';';);
+;c;f;.;k;e;y;_;p;r;e;s;s;(;';a;l;t;+;f;4;';);
+;c;f;.;k;e;y;_;p;r;e;s;s;(;';n;';);
