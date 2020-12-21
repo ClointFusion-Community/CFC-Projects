4ba1256529a1477ba821d4e26ff6edd05acc0882
ClointFusion-Community <ClointFusion@cloint.com>
2020-12-21 15:47:17 +0530

    RR BOT-108

---
+++ RR BOT-108_2020-12-21_15-47-17.py
@@ -0,0 +1,8 @@
+import ClointFusion as cf
+cf.launch_any_exe_bat_application('notepad')
+cf.key_write_enter('RR',key='')
+cf.key_press('shift')
+cf.key_hit_enter()
+cf.key_write_enter('HK',key='')
+cf.key_press('alt+f4')
+cf.key_press('n')
