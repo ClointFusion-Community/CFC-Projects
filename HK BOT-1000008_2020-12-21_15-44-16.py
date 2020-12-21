cd2f50e52b3913c302997b6488aaafde243b0e90
ClointFusion-Community <ClointFusion@cloint.com>
2020-12-21 15:44:16 +0530

    HK BOT-1000008

---
+++ HK BOT-1000008_2020-12-21_15-44-16.py
@@ -0,0 +1,7 @@
+import ClointFusion as cf
+cf.launch_any_exe_bat_application('notepad')
+cf.key_write_enter('R',key='')
+cf.key_press('shift')
+cf.key_write_enter('Ama',key='')
+cf.key_press('alt+f4')
+cf.key_press('n')
