bf565c667c3dfee263b87b681baf357bdfda01df
ClointFusion-Community <ClointFusion@cloint.com>
2020-12-29 08:27:27 +0530

    RR-BOT-OCR

---
+++ RR-BOT-OCR_2020-12-29_08-27-26.py
@@ -0,0 +1,9 @@
+import ClointFusion as cf
+cf.launch_any_exe_bat_application('notepad')
+cf.key_write_enter('RR',key='')
+cf.key_press('shift')
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_jiax0hen_generator\Images\Snips\2-UntitledNotepad-1527_466.png'),conf=0.7))
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_jiax0hen_generator\Images\Snips\3-UntitledNotepad-17_43.png'),conf=0.7))
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_jiax0hen_generator\Images\Snips\4-UntitledNotepad-58_245.png'),conf=0.7))
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_jiax0hen_generator\Images\Snips\5-Notepad-903_249.png'),conf=0.7))
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_jiax0hen_generator\Images\Snips\6-Notepad-1007_374.png'),conf=0.7))
