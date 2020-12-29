ce526e07b1539ec801682491debab539664a9049
ClointFusion-Community <ClointFusion@cloint.com>
2020-12-29 07:57:26 +0530

    RR-BOT-2

---
+++ RR-BOT-2_2020-12-29_07-57-26.py
@@ -0,0 +1,9 @@
+import ClointFusion as cf
+cf.launch_any_exe_bat_application('notepad')
+cf.key_hit_enter()
+cf.key_write_enter('RR',key='')
+cf.key_press('shift')
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_3n8zdxsj_generator\Images\Snips\2-UntitledNotepad-224_498.png'),conf=0.7))
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_3n8zdxsj_generator\Images\Snips\3-UntitledNotepad-262_696.png'),conf=0.7))
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_3n8zdxsj_generator\Images\Snips\4-Notepad-998_204.png'),conf=0.7))
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_3n8zdxsj_generator\Images\Snips\5-Notepad-982_320.png'),conf=0.7))
