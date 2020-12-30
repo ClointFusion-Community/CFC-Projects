96c465b4c698fe677277ec77933bb23119a16342
ClointFusion-Community <ClointFusion@cloint.com>
2020-12-30 07:24:54 +0530

    RR-BOT-OCR

---
+++ RR-BOT-OCR_2020-12-30_07-24-55.py
@@ -0,0 +1,9 @@
+import ClointFusion as cf
+cf.launch_any_exe_bat_application('search')
+cf.key_write_enter('notepad',key='')
+cf.key_write_enter('H',key='')
+cf.key_press('shift')
+cf.key_write_enter('Are',key='')
+cf.key_press('alt+f4')
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_5z0_dw4d_generator\Images\Snips\2-Notepad-949_285.png'),conf=0.7))
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_5z0_dw4d_generator\Images\Snips\3-Notepad-1034_397.png'),conf=0.7))
