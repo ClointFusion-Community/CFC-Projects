ea49248a7b4d63e122890322558156c773061558
ClointFusion-Community <ClointFusion@cloint.com>
2020-12-30 07:27:49 +0530

    RR-BOT-OCR

---
+++ RR-BOT-OCR_2020-12-30_07-27-50.py
@@ -0,0 +1,4 @@
+import ClointFusion as cf
+cf.launch_any_exe_bat_application('search')
+cf.key_write_enter('paint',key='')
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_1bsm5jw4_generator\Images\Snips\2-UntitledPaint-1902_15.png'),conf=0.7))
