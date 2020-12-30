5fda6d17ca328297e0225cfebe4b2080b28ab66e
ClointFusion-Community <ClointFusion@cloint.com>
2020-12-30 06:33:48 +0530

    RR-BOT-OCR

---
+++ RR-BOT-OCR_2020-12-30_06-33-48.py
@@ -0,0 +1,7 @@
+import ClointFusion as cf
+cf.launch_any_exe_bat_application('notepad')
+cf.key_press('shift')
+cf.key_write_enter('Dosa',key='')
+cf.key_press('alt+f4')
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_05bk2fp4_generator\Images\Snips\2-Notepad-961_224.png'),conf=0.7))
+cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(img=str(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_05bk2fp4_generator\Images\Snips\3-Notepad-1061_357.png'),conf=0.7))
