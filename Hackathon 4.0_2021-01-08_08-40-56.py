c6bb4808166104c01dedf23010fdc7292cf714a0
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-08 08:40:58 +0530

    Hackathon 4.0

---
+++ Hackathon 4.0_2021-01-08_08-40-56.py
@@ -0,0 +1,47 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_wsr874hs_generator\Images\Snips\1--340_39.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(340,39,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_wsr874hs_generator\Images\Snips\2--340_39.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(340,39,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_wsr874hs_generator\Images\Snips\3-NewTabGoogleChrome-390_85.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(390,85,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(1)
+cf.key_write_enter('clointfusion',key='')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(5)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_wsr874hs_generator\Images\Snips\4-WelcometoReadtheDocsClointFusionlatestdocumentationGoogleChrome-1864_66.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1864,66,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_wsr874hs_generator\Images\Snips\5-WelcometoReadtheDocsClointFusionlatestdocumentationGoogleChrome-1890_21.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1890,21,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.window_close_windows('Welcome to Read the Docs — ClointFusion latest documentation - Google Chrome')
