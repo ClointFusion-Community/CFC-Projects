043cfe422e76ba05b710fd6694724d7d64beae8d
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-08 07:16:34 +0530

    Hackathon 4.0

---
+++ Hackathon 4.0_2021-01-08_07-16-34.py
@@ -0,0 +1,41 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_7th5gfed_generator\Images\Snips\1--1457_343.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1457,343)
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_7th5gfed_generator\Images\Snips\2--248_944.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(248,944)
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_7th5gfed_generator\Images\Snips\3--248_944.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(248,944)
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_7th5gfed_generator\Images\Snips\4-NewTabGoogleChrome-379_74.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(379,74)
+time.sleep(1)
+cf.key_write_enter('modi')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_7th5gfed_generator\Images\Snips\5-modiGoogleSearchGoogleChrome-1904_53.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1904,53)
+time.sleep(0)
