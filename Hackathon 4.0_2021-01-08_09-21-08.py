1481319fdfb7598ce72bc91df0edeafe6419981e
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-08 09:21:09 +0530

    Hackathon 4.0

---
+++ Hackathon 4.0_2021-01-08_09-21-08.py
@@ -0,0 +1,58 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_hfz9x9xo_generator\Images\Snips\1--332_39.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(332,39,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_hfz9x9xo_generator\Images\Snips\2--332_39.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(332,39,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_hfz9x9xo_generator\Images\Snips\3-NewTabGoogleChrome-395_82.png',conf=0.7, wait=13),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(395,82,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(3)
+cf.key_write_enter('hare',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+cf.key_press('down')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_hfz9x9xo_generator\Images\Snips\4-harekrishnaGoogleSearchGoogleChrome-1918_70.png',conf=0.7, wait=13),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1918,70,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_hfz9x9xo_generator\Images\Snips\5-ProgramManager-1427_263.png',conf=0.7, wait=10),left_or_right='right', single_double_triple = 'single')
+except:
+    cf.mouse_click(1427,263,left_or_right='right', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_hfz9x9xo_generator\Images\Snips\6-ProgramManager-1453_333.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1453,333,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.window_close_windows('Program Manager')
