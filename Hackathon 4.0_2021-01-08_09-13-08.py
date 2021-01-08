0631fa307537ddeefd2513e4154769baec1c232c
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-08 09:13:10 +0530

    Hackathon 4.0

---
+++ Hackathon 4.0_2021-01-08_09-13-08.py
@@ -0,0 +1,54 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_y18lz6ln_generator\Images\Snips\1--336_39.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(336,39,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_y18lz6ln_generator\Images\Snips\2-NewTabGoogleChrome-336_39.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(336,39,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+cf.key_write_enter('radhe',key='')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(4)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_y18lz6ln_generator\Images\Snips\3-radheGoogleSearchGoogleChrome-395_79.png',conf=0.7, wait=13),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(395,79,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_y18lz6ln_generator\Images\Snips\4-NewTabGoogleChrome-1864_64.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1864,64,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_y18lz6ln_generator\Images\Snips\5-NewTabGoogleChrome-588_24.png',conf=0.7, wait=14),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(588,24,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(4)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_y18lz6ln_generator\Images\Snips\6-radheGoogleSearchGoogleChrome-1894_22.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1894,22,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.window_close_windows('radhe - Google Search - Google Chrome')
