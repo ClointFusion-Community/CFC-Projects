ffaa8b937ea64b0659c709acb71f102dbea2c91f
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-08 14:08:26 +0530

    Hackathon 4.0

---
+++ Hackathon 4.0_2021-01-08_14-08-24.py
@@ -0,0 +1,43 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_bclkds8j_generator\Images\Snips\1--1080_1048.png',conf=0.7, wait=19),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1080,1048,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(9)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_bclkds8j_generator\Images\Snips\2-ClointFusionBOTBuilderpyClointFusionBOTBuilderVisualStudioCode-153_617.png',conf=0.7, wait=36),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(153,617,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(26)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_bclkds8j_generator\Images\Snips\3--1018_1056.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1018,1056,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_bclkds8j_generator\Images\Snips\4-NewTabGoogleChrome-338_22.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(338,22,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_bclkds8j_generator\Images\Snips\5-NewTabGoogleChrome-1892_20.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1892,20,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.window_close_windows('New Tab - Google Chrome')
