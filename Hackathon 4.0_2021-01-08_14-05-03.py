5f05ef5225a7d0fe198f095158ff3dbe34f880a6
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-08 14:05:05 +0530

    Hackathon 4.0

---
+++ Hackathon 4.0_2021-01-08_14-05-03.py
@@ -0,0 +1,47 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_3n8u_1nw_generator\Images\Snips\1--338_41.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(338,41,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_3n8u_1nw_generator\Images\Snips\2--338_41.png',conf=0.7, wait=13),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(338,41,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_3n8u_1nw_generator\Images\Snips\3-NewTabGoogleChrome-338_21.png',conf=0.7, wait=13),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(338,21,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(3)
+cf.key_write_enter('modiji',key='')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(29)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_3n8u_1nw_generator\Images\Snips\4-modijiGoogleSearchGoogleChrome-1889_18.png',conf=0.7, wait=15),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1889,18,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(5)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_3n8u_1nw_generator\Images\Snips\5--1078_1043.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1078,1043,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.window_close_windows('nan')
