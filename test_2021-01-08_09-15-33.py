0405e32d5866d805189bfb988279c7e25e33ec58
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-08 09:15:24 +0530

    test

---
+++ test_2021-01-08_09-15-33.py
@@ -0,0 +1,60 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Karth\AppData\Local\Temp\cf_log_2dm66eov_generator\Images\Snips\1--566_853.png',conf=0.7, wait=12),left_or_right='right', single_double_triple = 'single')
+except:
+    cf.mouse_click(566,853,left_or_right='right', single_double_triple = 'single')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Karth\AppData\Local\Temp\cf_log_2dm66eov_generator\Images\Snips\2--672_603.png',conf=0.7, wait=14),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(672,603,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(4)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Karth\AppData\Local\Temp\cf_log_2dm66eov_generator\Images\Snips\3--316_1059.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(316,1059,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(1)
+cf.key_write_enter('chr',key='')
+time.sleep(1)
+cf.key_press('enter')
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Karth\AppData\Local\Temp\cf_log_2dm66eov_generator\Images\Snips\4-NewTabGoogleChrome-551_94.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(551,94,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Karth\AppData\Local\Temp\cf_log_2dm66eov_generator\Images\Snips\5-NewTabGoogleChrome-551_94.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(551,94,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(1)
+cf.key_write_enter('goo',key='')
+time.sleep(1)
+cf.key_press('right')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(5)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Karth\AppData\Local\Temp\cf_log_2dm66eov_generator\Images\Snips\6-GoogleGoogleChrome-1919_0.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1919,0,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.window_close_windows('Google - Google Chrome')
