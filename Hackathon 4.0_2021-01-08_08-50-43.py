aeea6bd3fda55d082f2a6110564f89d079df0f6e
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-08 08:50:44 +0530

    Hackathon 4.0

---
+++ Hackathon 4.0_2021-01-08_08-50-43.py
@@ -0,0 +1,40 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_ksohsu92_generator\Images\Snips\1--1739_486.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1739,486,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_ksohsu92_generator\Images\Snips\2--325_34.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(325,34,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_ksohsu92_generator\Images\Snips\3--325_34.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(325,34,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(1)
+cf.key_write_enter('krishna',key='')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(14)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_ksohsu92_generator\Images\Snips\4-KrishnacomAllAboutKrishnaGoogleChrome-1907_66.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1907,66,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.window_close_windows('Krishna.com | All About Krishna - Google Chrome')
