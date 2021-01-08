db0a89852b7ee9f38ff22aeb71f9fae33d43d37d
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-08 07:41:19 +0530

    Hackathon 4.0

---
+++ Hackathon 4.0_2021-01-08_07-41-18.py
@@ -0,0 +1,50 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_z36qdvib_generator\Images\Snips\1--1661_290.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1661,290,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_z36qdvib_generator\Images\Snips\2--236_948.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(236,948,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_z36qdvib_generator\Images\Snips\3--236_948.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(236,948,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_z36qdvib_generator\Images\Snips\4-NewTabGoogleChrome-387_67.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(387,67,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(1)
+cf.key_write_enter('hare',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('krishna',key='')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_z36qdvib_generator\Images\Snips\5-harekrishnaGoogleSearchGoogleChrome-1904_60.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1904,60,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
