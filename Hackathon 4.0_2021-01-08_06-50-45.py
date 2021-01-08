7d1d71d29ebf1a5cf82fd2de132724e4923bc310
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-08 06:50:46 +0530

    Hackathon 4.0

---
+++ Hackathon 4.0_2021-01-08_06-50-45.py
@@ -0,0 +1,7 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
