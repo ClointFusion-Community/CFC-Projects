f12c9d63b4c99e2373bd6ddee5074fdc4d87632b
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-08 07:51:06 +0530

    Hackathon 4.0

---
+++ Hackathon 4.0_2021-01-08_07-50-35.py
@@ -0,0 +1,55 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_4_bwe4kq_generator\Images\Snips\1--1598_428.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1598,428,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_4_bwe4kq_generator\Images\Snips\2--235_938.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(235,938,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_4_bwe4kq_generator\Images\Snips\3--235_938.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(235,938,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_4_bwe4kq_generator\Images\Snips\4-NewTabGoogleChrome-387_78.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(387,78,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_4_bwe4kq_generator\Images\Snips\5-NewTabGoogleChrome-696_79.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(696,79,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(1)
+cf.key_write_enter('hare',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+cf.key_press('enter')
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_4_bwe4kq_generator\Images\Snips\6-hareGoogleSearchGoogleChrome-1910_69.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1910,69,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
