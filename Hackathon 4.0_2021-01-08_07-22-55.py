7182a9b8fb7a4785b4caae405d920807684ad82a
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-08 07:22:56 +0530

    Hackathon 4.0

---
+++ Hackathon 4.0_2021-01-08_07-22-55.py
@@ -0,0 +1,41 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_5fa2gg4s_generator\Images\Snips\1--1788_368.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1788,368,left_or_right='left', single_double_triple = 'single')
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_5fa2gg4s_generator\Images\Snips\2--246_938.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(246,938,left_or_right='left', single_double_triple = 'single')
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_5fa2gg4s_generator\Images\Snips\3--246_938.png',conf=0.7, wait=13),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(246,938,left_or_right='left', single_double_triple = 'double')
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_5fa2gg4s_generator\Images\Snips\4-NewTabGoogleChrome-385_77.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(385,77,left_or_right='left', single_double_triple = 'single')
+time.sleep(1)
+cf.key_write_enter('modi')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_5fa2gg4s_generator\Images\Snips\5-modiGoogleSearchGoogleChrome-1905_57.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1905,57,left_or_right='left', single_double_triple = 'single')
+time.sleep(0)
