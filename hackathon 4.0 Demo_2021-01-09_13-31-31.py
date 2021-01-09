b2a9dcfe3bcea574a1f1750fc081721203cee152
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-09 13:31:31 +0530

    hackathon 4.0 Demo

---
+++ hackathon 4.0 Demo_2021-01-09_13-31-31.py
@@ -0,0 +1,47 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_gu0z2qio_generator\Images\Snips\1--240_558.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(240,558,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_gu0z2qio_generator\Images\Snips\2--240_558.png',conf=0.7, wait=13),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(240,558,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_gu0z2qio_generator\Images\Snips\3-NewTabGoogleChrome-407_252.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(407,252,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(1)
+cf.key_write_enter('modi',key='')
+time.sleep(1)
+cf.key_press('enter')
+time.sleep(4)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_gu0z2qio_generator\Images\Snips\4-modiGoogleSearchGoogleChrome-1889_244.png',conf=0.7, wait=16),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1889,244,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(6)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\mrmay\AppData\Local\Temp\cf_log_gu0z2qio_generator\Images\Snips\5-modiGoogleSearchGoogleChrome-1892_14.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1892,14,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.window_close_windows('modi - Google Search - Google Chrome')
