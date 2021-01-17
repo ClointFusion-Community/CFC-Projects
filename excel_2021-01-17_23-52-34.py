493ffdc6bea8a2d0f0f658e6f0c9ee23ac5f1a0a
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-17 23:52:35 +0530

    excel

---
+++ excel_2021-01-17_23-52-34.py
@@ -0,0 +1,44 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+cf.key_press('cmd')
+time.sleep(1)
+cf.key_write_enter('note',key='')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(2)
+cf.key_write_enter('hi',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('sushil',key='')
+time.sleep(0)
+cf.key_press(ctrl+Z)
+cf.key_press(ctrl+X)
+cf.key_press(ctrl+C)
+cf.key_press(ctrl+V)
+
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('how',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('ru',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(2)
+
+
+time.sleep(1)
+cf.key_press('alt')
+time.sleep(0)
+cf.key_press('f4')
+time.sleep(1)
+cf.key_write_enter('n',key='')
+time.sleep(0)
