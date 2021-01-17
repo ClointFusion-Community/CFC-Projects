0c5efd698b0f697a3cfa95119bae7f87a063afc7
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-17 23:43:36 +0530

    excel

---
+++ excel_2021-01-17_23-43-35.py
@@ -0,0 +1,58 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+cf.key_press('cmd')
+time.sleep(0)
+nan
+time.sleep(0)
+cf.key_press(ctrl+Z)
+cf.key_press(ctrl+X)
+cf.key_press(ctrl+C)
+cf.key_press(ctrl+V)
+cf.key_press(ctrl+A)
+cf.key_press(ctrl+S)
+
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(2)
+nan
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+nan
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+nan
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+nan
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+
+
+time.sleep(1)
+cf.key_press('f4')
+time.sleep(1)
+cf.key_press('alt')
+time.sleep(0)
+cf.key_press('alt')
+time.sleep(0)
+cf.key_press('f4')
+time.sleep(1)
+nan
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_r8mrht0e_generator\Images\Snips\1--193_339.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(193,339,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.window_close_windows('nan')
