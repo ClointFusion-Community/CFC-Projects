77e96c6c6fc47a259d7ff9acf5541508ced8485c
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-18 14:17:29 +0530

    excel

---
+++ excel_2021-01-18_14-17-28.py
@@ -0,0 +1,85 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_ba3soo1b_generator\Images\Snips\1--557_323.png',conf=0.7, wait=13),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(557,323,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(3)
+cf.key_write_enter('ho',key='')
+time.sleep(0)
+cf.key_press('backspace')
+time.sleep(0)
+cf.key_press('backspace')
+time.sleep(1)
+cf.key_write_enter('not',key='')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(2)
+cf.key_write_enter('notepad',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(2)
+cf.key_write_enter('hi',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+cf.key_write_enter('sushil',key='')
+time.sleep(0)
+cf.key_press('ctrl+A')
+cf.key_press('ctrl+Z')
+cf.key_press('ctrl+X')
+cf.key_press('ctrl+C')
+cf.key_press('ctrl+V')
+
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('how',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('ru',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+cf.key_press('alt')
+time.sleep(1)
+cf.key_press('alt')
+time.sleep(1)
+
+
+time.sleep(2)
+cf.key_press('f4')
+time.sleep(0)
+cf.key_press('alt')
+time.sleep(1)
+cf.key_write_enter('n',key='')
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_ba3soo1b_generator\Images\Snips\2-ProgramManager-788_249.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(788,249,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_ba3soo1b_generator\Images\Snips\3-ProgramManager-788_249.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(788,249,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.window_close_windows('Program Manager')
