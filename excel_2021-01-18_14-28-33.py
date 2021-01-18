cbab7c6d8ea9e110ec1179e67d564d39c60d3ae9
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-18 14:28:34 +0530

    excel

---
+++ excel_2021-01-18_14-28-33.py
@@ -0,0 +1,62 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+cf.key_press('cmd')
+time.sleep(1)
+cf.key_write_enter('note',key='')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(1)
+cf.key_write_enter('hi',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_press('ctrl+A')
+cf.key_press('ctrl+X')
+cf.key_press('ctrl+C')
+cf.key_press('ctrl+V')
+cf.key_press('ctrl+Z')
+
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('how',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('r',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('u',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+
+
+time.sleep(1)
+cf.key_press('f4')
+time.sleep(0)
+cf.key_press('alt')
+time.sleep(0)
+cf.key_write_enter('bn',key='')
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_72zfggun_generator\Images\Snips\1-ProgramManager-320_217.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(320,217,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_72zfggun_generator\Images\Snips\2-ProgramManager-320_217.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(320,217,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.window_close_windows('Program Manager')
