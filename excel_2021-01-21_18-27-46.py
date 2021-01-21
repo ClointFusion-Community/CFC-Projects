747230e1f90a75532246465e288908ba5728f814
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-21 18:27:45 +0530

    excel

---
+++ excel_2021-01-21_18-27-46.py
@@ -0,0 +1,53 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_gkv4l5f4_generator\Images\Snips\1--636_335.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(636,335,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(1)
+cf.key_write_enter('note',key='')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(1)
+cf.key_write_enter('hi',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+cf.key_write_enter('sushil',key='')
+time.sleep(0)
+cf.key_press('ctrl+A')
+cf.key_press('ctrl+X')
+cf.key_press('ctrl+C')
+
+time.sleep(1)
+cf.key_write_enter('how',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('r',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+cf.key_write_enter('u',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+
+
+time.sleep(0)
+cf.key_write_enter('v',key='')
+time.sleep(2)
+cf.key_press('alt+f4')
+time.sleep(0)
+
+
+time.sleep(0)
+cf.key_write_enter('n',key='')
+time.sleep(0)
