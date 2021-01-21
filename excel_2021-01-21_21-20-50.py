04a8aa32863f602a68a44f29d72c8557d65ec8e3
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-21 21:20:49 +0530

    excel

---
+++ excel_2021-01-21_21-20-50.py
@@ -0,0 +1,46 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_hgz1jbng_generator\Images\Snips\1--618_439.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(618,439,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(1)
+
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+
+time.sleep(0)
+cf.key_press('backspace')
+time.sleep(1)
+
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+
+time.sleep(0)
+cf.key_press('space')
+time.sleep(1)
+cf.key_press('alt+f4')
+time.sleep(0)
+
+
+time.sleep(4)
+
+
+time.sleep(0)
