d75383ddc3b147b5c997732a44baa3215fbd98b9
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-22 10:19:12 +0530

    excel

---
+++ excel_2021-01-22_10-19-13.py
@@ -0,0 +1,55 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_iv8cgz1z_generator\Images\Snips\1--261_749.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(261,749,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+cf.key_write_enter('note',key='')
+time.sleep(0)
+cf.key_press('ctrl+A')
+cf.key_press('ctrl+Z')
+cf.key_press('ctrl+Y')
+cf.key_press('ctrl+Z')
+cf.key_press('ctrl+X')
+cf.key_press('ctrl+V')
+time.sleep(1)
+cf.key_press('enter')
+time.sleep(4)
+cf.key_write_enter('H',key='')
+time.sleep(0)
+cf.key_press('shift')
+time.sleep(0)
+cf.key_write_enter('are',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('K',key='')
+time.sleep(0)
+cf.key_press('shift')
+time.sleep(0)
+cf.key_write_enter('rishna',key='')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(21)
+
+
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_iv8cgz1z_generator\Images\Snips\2-UntitledNotepad-1097_175.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(1097,175,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(1)
+cf.key_write_enter('n',key='')
+time.sleep(0)
