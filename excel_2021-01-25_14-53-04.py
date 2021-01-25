b998c20b0182496c377db22abaeb5d1dd8d9a347
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-25 14:53:05 +0530

    excel

---
+++ excel_2021-01-25_14-53-04.py
@@ -0,0 +1,191 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+cf.key_write_enter('ote',key='')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(1)
+cf.key_write_enter('hi',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('sushil',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('how',key='')
+time.sleep(0)
+cf.key_press('space')
+time.sleep(0)
+cf.key_write_enter('ru',key='')
+time.sleep(1)
+cf.key_press('space')
+time.sleep(1)
+cf.key_press('alt+tab')
+time.sleep(1)
+cf.key_press('alt+tab')
+time.sleep(1)
+cf.key_press('alt+tab')
+time.sleep(0)
+
+
+time.sleep(1)
+cf.key_press('tab')
+time.sleep(0)
+cf.key_press('alt+tab')
+time.sleep(0)
+cf.key_press('alt+alt')
+time.sleep(2)
+cf.key_press('alt+tab')
+time.sleep(0)
+
+
+time.sleep(1)
+cf.key_press('alt+tab')
+time.sleep(0)
+cf.key_press('alt+tab')
+time.sleep(2)
+
+
+time.sleep(1)
+cf.key_press('alt+right')
+time.sleep(0)
+
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_c6_0kd3c_generator\Images\Snips\1-UntitledNotepad-595_441.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(595,441,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(1)
+cf.key_press('alt+tab')
+time.sleep(0)
+
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_c6_0kd3c_generator\Images\Snips\2-ClointFusionBOTBuilderpyVisualStudioCode-694_379.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(694,379,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_c6_0kd3c_generator\Images\Snips\3-ClointFusionBOTBuilderpyVisualStudioCode-702_407.png',conf=0.7, wait=14),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(702,407,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(4)
+cf.key_press('alt+tab')
+time.sleep(0)
+cf.key_press('alt+tab')
+time.sleep(1)
+cf.key_press('alt+tab')
+time.sleep(0)
+cf.key_press('alt+tab')
+time.sleep(1)
+cf.key_press('alt+tab')
+time.sleep(0)
+cf.key_press('alt+tab')
+time.sleep(1)
+cf.key_press('alt+tab')
+time.sleep(0)
+cf.key_press('alt+tab')
+time.sleep(0)
+cf.key_press('alt+tab')
+time.sleep(0)
+
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_c6_0kd3c_generator\Images\Snips\4-UntitledNotepad-1151_114.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(1151,114,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(0)
+cf.key_press('alt+tab')
+time.sleep(0)
+cf.key_press('alt+tab')
+time.sleep(2)
+cf.key_press('alt+tab')
+time.sleep(0)
+
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_c6_0kd3c_generator\Images\Snips\5-UntitledNotepad-1365_0.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(1365,0,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(1)
+cf.key_press('right')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_c6_0kd3c_generator\Images\Snips\6-ClointFusionBOTBuilderpyVisualStudioCode-831_284.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(831,284,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_c6_0kd3c_generator\Images\Snips\7-ClointFusionBOTBuilderpyVisualStudioCode-828_345.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(828,345,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_c6_0kd3c_generator\Images\Snips\8-ClointFusionBOTBuilderpyVisualStudioCode-804_410.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(804,410,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_c6_0kd3c_generator\Images\Snips\9-ClointFusionBOTBuilderpyVisualStudioCode-810_366.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(810,366,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_c6_0kd3c_generator\Images\Snips\10-ClointFusionBOTBuilderpyVisualStudioCode-825_344.png',conf=0.7, wait=15),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(825,344,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(5)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_c6_0kd3c_generator\Images\Snips\11-ClointFusionBOTBuilderpyVisualStudioCode-841_350.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(841,350,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+cf.key_press('alt+tab')
+time.sleep(0)
+
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_c6_0kd3c_generator\Images\Snips\12-sbtestLoginRequestjava-844_333.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(844,333,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.key_press('alt')
+time.sleep(0)
+cf.key_press('tab')
+time.sleep(0)
