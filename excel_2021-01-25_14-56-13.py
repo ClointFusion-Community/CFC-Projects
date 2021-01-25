3a7c35200c1ef2f5da826442229496854075dfd3
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-25 14:56:14 +0530

    excel

---
+++ excel_2021-01-25_14-56-13.py
@@ -0,0 +1,73 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+cf.key_press('alt+tab')
+time.sleep(0)
+
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_qpp801ov_generator\Images\Snips\1-cflogqpp801ovgenerator-551_221.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(551,221,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_qpp801ov_generator\Images\Snips\2-cflogqpp801ovgenerator-551_221.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(551,221,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(1)
+cf.key_press('down')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(4)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_qpp801ov_generator\Images\Snips\3-Snips-692_439.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(692,439,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_qpp801ov_generator\Images\Snips\4-Snips-692_439.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(692,439,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+cf.key_press('alt+tab')
+time.sleep(0)
+
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_qpp801ov_generator\Images\Snips\5-ClointFusionBOTBuilderpyVisualStudioCode-692_439.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(692,439,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.key_press('alt+tab')
+time.sleep(0)
+cf.key_press('alt+tab')
+time.sleep(4)
+cf.key_press('alt+tab')
+time.sleep(0)
+
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_qpp801ov_generator\Images\Snips\6-ClointFusionBOTBuilderpyVisualStudioCode-692_439.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(692,439,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+cf.window_close_windows('ClointFusion_BOT_Builder.py - Visual Studio Code')
