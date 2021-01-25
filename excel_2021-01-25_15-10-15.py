a1a69597c725e8a073f1fce4972823ba830a8e19
ClointFusion-Community <ClointFusion@cloint.com>
2021-01-25 15:10:16 +0530

    excel

---
+++ excel_2021-01-25_15-10-15.py
@@ -0,0 +1,114 @@
+# This code is generated automatically by ClointFusion BOT Builder Tool.
+import ClointFusion as cf 
+import time 
+
+cf.window_show_desktop() 
+cf.mouse_click(int(cf.pg.size()[0]/2),int(cf.pg.size()[1]/2))
+
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_djfih_un_generator\Images\Snips\1--696_742.png',conf=0.7, wait=13),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(696,742,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(3)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_djfih_un_generator\Images\Snips\2--511_740.png',conf=0.7, wait=14),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(511,740,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(4)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_djfih_un_generator\Images\Snips\3--601_605.png',conf=0.7, wait=11),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(601,605,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_djfih_un_generator\Images\Snips\4--706_597.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(706,597,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_djfih_un_generator\Images\Snips\5--703_754.png',conf=0.7, wait=17),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(703,754,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(7)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_djfih_un_generator\Images\Snips\6-ClointFusionBOTBuilderpyVisualStudioCode-68_648.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(68,648,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(0)
+cf.key_press('ctrl+V')
+time.sleep(1)
+
+
+time.sleep(0)
+cf.key_press('cmd')
+time.sleep(1)
+cf.key_press('enter')
+time.sleep(1)
+cf.key_press('down')
+time.sleep(0)
+cf.key_press('down')
+time.sleep(0)
+cf.key_press('up')
+time.sleep(1)
+cf.key_press('up')
+time.sleep(1)
+cf.key_press('cmd')
+time.sleep(0)
+cf.key_press('down')
+time.sleep(0)
+cf.key_press('up')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(1)
+cf.key_press('down')
+time.sleep(0)
+cf.key_press('down')
+time.sleep(0)
+cf.key_press('enter')
+time.sleep(1)
+cf.key_press('down')
+time.sleep(1)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_djfih_un_generator\Images\Snips\7-Snips-667_621.png',conf=0.7, wait=12),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(667,621,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(2)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_djfih_un_generator\Images\Snips\8-Snips-667_621.png',conf=0.7, wait=22),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(667,621,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(12)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_djfih_un_generator\Images\Snips\9-Snips-667_621.png',conf=0.7, wait=10),left_or_right='left', single_double_triple = 'single')
+except:
+    cf.mouse_click(667,621,left_or_right='left', single_double_triple = 'single')
+
+time.sleep(0)
+
+try:
+    cf.mouse_click(*cf.mouse_search_snip_return_coordinates_x_y(r'C:\Users\Sushil\AppData\Local\Temp\cf_log_djfih_un_generator\Images\Snips\10-Snips-667_621.png',conf=0.7, wait=13),left_or_right='left', single_double_triple = 'double')
+except:
+    cf.mouse_click(667,621,left_or_right='left', single_double_triple = 'double')
+
+time.sleep(3)
+cf.key_press('alt')
+time.sleep(0)
+cf.key_press('tab')
+time.sleep(0)
